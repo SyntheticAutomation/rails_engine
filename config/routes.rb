6d3b29483d0b8b37022e3ccdfbeea3297ef86f60
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-24 14:10:18 -0700

    Adjusts route to be restful

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/most_revenue', to: 'most_revenue#index'
       end
+      namespace :customers do
+        get '/find', to: 'search#show'
+      end
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
       resources :merchants, only: [:index, :show]
-
       resources :transactions, only: [:index, :show]
     end
   end
