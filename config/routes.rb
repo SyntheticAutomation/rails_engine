ef0ca850ba1fc52b4330d4816533fcea24b3f55c
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-23 22:28:13 -0700

    Adds most_revenue route from project spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/most_revenue', to: 'most_revenue#index'
+      end
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
       resources :merchants, only: [:index, :show]
+
       resources :transactions, only: [:index, :show]
     end
   end
-
 
 end
