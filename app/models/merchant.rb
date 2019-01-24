ea358dd398f63ca498ea46c7121867834710f293
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-23 22:28:47 -0700

    Adds controller for new statistic and adds top_profiteers method with a limit

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,7 +3,11 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
 
-  def self.top_profiteers(limit)
+  def self.top_profiteers(limit = 0)
+    joins(invoices: {:items => :invoice_items} )
+    .group("merchants.id")
+    .order("revenue DESC")
+    .select("merchants.*, sum(invoice_items.unit_price) as revenue")
+    .limit(limit)
   end
-
 end
