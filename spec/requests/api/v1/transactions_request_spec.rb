a0520ac731e477256f0346aba586cfe2e1f5b8c3
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-24 16:08:37 -0700

    Adds test for find_all

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -25,7 +25,7 @@ describe 'Transactions API' do
           credit_card_number: 4242424242421234,
           result: "success"
         )
-        
+
     query_params = [ 'id=123',
                'credit_card_number=4242424242421234',
                'result=success'
@@ -36,5 +36,23 @@ describe 'Transactions API' do
       transaction = JSON.parse(response.body)
       expect(transaction["data"]["attributes"]["id"]).to eq(123)
     end
+  end
+  it 'can find multiple transactions by different attributes' do
+    create(:transaction, id: 123, credit_card_number: 4242424242421234, result: "success")
+    create(:transaction, id: 400, credit_card_number: 1111111111111111, result: "success")
+    create(:transaction)
+
+    get '/api/v1/transactions/find_all?result=success'
+
+    matching_transactions = (JSON.parse(response.body))["data"]
+    transaction_1_attributes = matching_transactions[0]["attributes"]
+    transaction_2_attributes = matching_transactions[1]["attributes"]
+
+    10.times do
+      expect(matching_transactions.sample["attributes"]["result"]).to eq("success")
+    end
+
+    expect(transaction_1_attributes["id"]).to eq(123)
+    expect(transaction_2_attributes["id"]).to eq(400)
   end
 end
