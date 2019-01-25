cf091a8087f09be7f519529776d3c6c7645438bb
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-25 09:39:58 -0700

    Cleans up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   tables = [:merchants,
             :customers,
             :invoices,
-            :items,
             :invoice_items,
+            :items,
             :transactions
           ]
 
@@ -18,12 +18,33 @@ Rails.application.routes.draw do
           get '/most_revenue', to: 'most_revenue#index' if table == :merchants
         end
       end
-      resources :customers, only: [:index, :show]
-      resources :invoices, only: [:index, :show]
-      resources :items, only: [:index, :show]
-      resources :invoice_items, only: [:index, :show]
-      resources :merchants, only: [:index, :show]
-      resources :transactions, only: [:index, :show]
+
+      resources :customers, only: [:index, :show] do
+        resources :invoices, only: [:index]
+        resources :transactions, only: [:index]
+      end
+      resources :invoices, only: [:index, :show] do
+        resources :transactions, only: [:index]
+        resources :invoice_items, only: [:index]
+        resources :items, only: [:index]
+        resource :customer, only: [:show]
+        resource :merchant, only: [:show]
+      end
+      resources :items, only: [:index, :show] do
+        resources :invoice_items, only: [:index]
+        resource :merchant, only: [:show]
+      end
+      resources :invoice_items, only: [:index, :show] do
+        resource :invoice, only: [:show]
+        resource :item, only: [:show]
+      end
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index]
+        resources :invoices, only: [:index]
+      end
+      resources :transactions, only: [:index, :show] do
+        resource :invoice, only: [:show]
+      end
     end
   end
 end
