4752f2f01f4a0ee6dbe778fa0ea79805cdd623b5
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-26 11:28:27 -0700

    Adds top_item_sellers method and controller

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,7 +7,15 @@ class Merchant < ApplicationRecord
     joins(invoices: {:items => :invoice_items} )
     .group("merchants.id")
     .order("revenue DESC")
-    .select("merchants.*, sum(invoice_items.unit_price) as revenue")
+    .select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as revenue")
+    .limit(limit)
+  end
+
+  def self.top_item_sellers(limit = 0)
+    joins(invoices: {:items => :invoice_items} )
+    .group("merchants.id")
+    .order("items_sold DESC")
+    .select("merchants.*, sum(invoice_items.quantity) as items_sold")
     .limit(limit)
   end
 end
